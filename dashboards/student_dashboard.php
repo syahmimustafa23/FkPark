69bd63b6d0af2683bf1c5e9acfeca146379bafac
Haziq Muttaqien <ayiehaziq58@gmail.com>
2025-12-23 15:28:15 +0800

    update student

--- dashboards/student_dashboard.php
+++ dashboards/student_dashboard.php
@@ -120,7 +120,7 @@ td{
         <a href="#home"><img class="logo" src="../photo/logoUmpsa.png"></a>
         <a class="sidebar2" href="../Module2/student_view.php">View Parking</a>
         <a class="sidebar2" href="../Module 3/view_bookings.php">View Booking</a>
-        <a class="sidebar2" href="../Module4/view-status.php">Traffic Summons</a>
+        <a class="sidebar2" href="../Module4/view-status.php">View Update Point & Status</a>
     </div>
 
     
